532eef4e7787b20191c925c6457ea377d903ea51
Shaurya-GitH <mehta.shaurya123@gmail.com>
2025-03-25 21:44:17 +0530

    Adding Session Handling with Dynamic Pages

--- src/main/java/org/example/springbootmvc/controllers/MyController.java
+++ src/main/java/org/example/springbootmvc/controllers/MyController.java
@@ -4,11 +4,16 @@ import org.example.springbootmvc.entities.Users;
 import org.example.springbootmvc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("name")
 public class MyController {
     private UserService userService;
     @Autowired
@@ -31,13 +36,23 @@ public class MyController {
         return "home";
     }
 
+    @ModelAttribute
+    String storeName(){
+        return "";
+    }
+
     @PostMapping("/checkLogin")
-    String check(@ModelAttribute Users user){
+    ModelAndView check(@ModelAttribute Users user, Model model){
         if(userService.checkLogin(user)){
-            return "redirect:/home";
+            model.addAttribute("name",user.getName());
+            ModelAndView mav=new ModelAndView("redirect:home");
+            return mav;
         }
         else{
-            return "redirect:/login";
+            ModelAndView mav=new ModelAndView("login");
+            boolean error=true;
+            mav.addObject("errorMessage",error);
+            return mav;
         }
     }
 
@@ -45,5 +60,11 @@ public class MyController {
     String registerUser(@ModelAttribute Users user){
         if(userService.registerUser(user))return "redirect:/login";
         else return "redirect:/register";
+    }
+
+    @GetMapping("/logout")
+    String logOut(SessionStatus status){
+        status.setComplete();
+        return "redirect:/";
     }
 }
